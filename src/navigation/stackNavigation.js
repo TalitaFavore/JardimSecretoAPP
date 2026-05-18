c785bf68fcc3d1f8360804041405b2bdc817dd5e
TalitaFavore <talitafavore@gmail.com>
2026-05-18 18:12:02 -0300

    feat: adicionar tela de login

--- src/navigation/stackNavigation.js
+++ src/navigation/stackNavigation.js
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import SplashScreen from "../screens/SplashScreen";
 import TabNavigation from "./tabNavigation";
@@ -17,6 +18,12 @@ export default function StackNavigation() {
       <Stack.Screen
         name="Cadastro"
         component={RegisterScreen}
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
         options={{ headerShown: false }}
       />
 
