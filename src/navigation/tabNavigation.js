9474bdf125a39cfe634751c9bea2ea4bee027af7
TalitaFavore <talitafavore@gmail.com>
2026-05-25 20:40:08 -0300

    feat: adicionar tela de pesquisa de produtos

--- src/navigation/tabNavigation.js
+++ src/navigation/tabNavigation.js
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
+import SearchScreen from "../screens/SearchScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -7,6 +8,7 @@ export default function TabNavigation() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Pesquisa" component={SearchScreen} />
     </Tab.Navigator>
   );
 }
